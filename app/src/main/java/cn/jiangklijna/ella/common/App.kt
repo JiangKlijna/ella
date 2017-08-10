6a3ed322215577aaf333ea0ef53f28427b8cbeff
jiangKlijna <leil729564090@outlook.com>
2017-08-10 10:00:28 +0800

    :fire:ActMain App

---
+++ app/src/main/java/cn/jiangklijna/ella/common/App.kt
@@ -0,0 +1,14 @@
+package cn.jiangklijna.ella.common
+
+import android.app.Application
+
+/**
+ * Created by jiangKlijna on 8/10/2017.
+ */
+class App : Application() {
+
+	override fun onCreate() {
+		super.onCreate()
+
+	}
+}
---
+++ app/src/main/java/cn/jiangklijna/ella/ui/activity/ActMain.kt
@@ -0,0 +1,16 @@
+package cn.jiangklijna.ella.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by jiangKlijna on 8/10/2017.
+ */
+class ActMain : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+	}
+
+}
