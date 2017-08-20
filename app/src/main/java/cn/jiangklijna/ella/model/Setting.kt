3e776cb89c5545fbc31bd288ef7d6f7ce0657a30
jiangKlijna <leil729564090@outlook.com>
2017-08-20 14:05:58 +0800

    :fire:add setting url

--- app/src/main/java/cn/jiangklijna/ella/model/Setting.kt
+++ app/src/main/java/cn/jiangklijna/ella/model/Setting.kt
@@ -10,18 +10,19 @@ import cn.jiangklijna.ella.ui.fragment.FrgEnglishCards
 object Setting {
 
 	private val frgs = arrayOf(
-			Data(0, "VOA慢速", FrgEcHtml::class.java),
-			Data(1, "VOA一分钟", FrgEcHtml::class.java),
-			Data(2, "VOA视频", FrgEcHtml::class.java),
-			Data(3, "BBC六分钟", FrgEcHtml::class.java),
-			Data(4, "BBC地道英语", FrgEcHtml::class.java),
-			Data(5, "BBC新闻", FrgEcHtml::class.java),
-			Data(6, "TED演讲", FrgEcJson::class.java)
+			Data(0, "VOA慢速", "http://m.iyuba.com/voaS/index.jsp", FrgEcHtml::class.java),
+			Data(1, "VOA一分钟", "http://m.iyuba.com/voaS/indexAM.jsp", FrgEcHtml::class.java),
+			Data(2, "VOA视频", "http://m.iyuba.com/voaS/indexCV.jsp", FrgEcHtml::class.java),
+			Data(3, "BBC六分钟", "http://m.iyuba.com/bbcwap/index.jsp?parentID=1", FrgEcHtml::class.java),
+			Data(4, "BBC地道英语", "http://m.iyuba.com/bbcwap/index.jsp?parentID=5", FrgEcHtml::class.java),
+			Data(5, "BBC新闻", "http://m.iyuba.com/bbcwap/index.jsp?parentID=3", FrgEcHtml::class.java),
+			Data(6, "TED演讲", "http://apps.iyuba.com/voa/titleTed.jsp?maxid=0&type=mobileweb&pageNum=6&format=json&pages=1", FrgEcJson::class.java)
 	)
 
 	data class Data(
 			val id: Int,
 			val title: String,
+			val url: String,
 			val frgclass: Class<out FrgEnglishCards>
 	)
 
