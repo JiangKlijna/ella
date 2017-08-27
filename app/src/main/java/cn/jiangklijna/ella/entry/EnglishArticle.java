1b6dddb0e7f43a3b0e4f9ae884297aaa2c3dd7d3
jiangKlijna <leil729564090@outlook.com>
2017-08-27 15:13:50 +0800

    :fire:add toString

--- app/src/main/java/cn/jiangklijna/ella/entry/EnglishArticle.java
+++ app/src/main/java/cn/jiangklijna/ella/entry/EnglishArticle.java
@@ -10,61 +10,84 @@ import org.greenrobot.greendao.annotation.Generated;
 @Entity
 public class EnglishArticle {
 
-    @Id
-    private int id;
-    private String title;
-    private String url;
-    private String img;
-    private String date;
-    private int flag;
-    @Generated(hash = 2078708353)
-    public EnglishArticle(int id, String title, String url, String img, String date,
-                          int flag) {
-        this.id = id;
-        this.title = title;
-        this.url = url;
-        this.img = img;
-        this.date = date;
-        this.flag = flag;
-    }
-    @Generated(hash = 1421194240)
-    public EnglishArticle() {
-    }
-    public int getId() {
-        return this.id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getTitle() {
-        return this.title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getUrl() {
-        return this.url;
-    }
-    public void setUrl(String url) {
-        this.url = url;
-    }
-    public String getImg() {
-        return this.img;
-    }
-    public void setImg(String img) {
-        this.img = img;
-    }
-    public String getDate() {
-        return this.date;
-    }
-    public void setDate(String date) {
-        this.date = date;
-    }
-    public int getFlag() {
-        return this.flag;
-    }
-    public void setFlag(int flag) {
-        this.flag = flag;
-    }
+	@Id
+	private int id;
+	private String title;
+	private String url;
+	private String img;
+	private String date;
+	private int flag;
+	@Override
+	public String toString() {
+		return "EnglishArticle{" +
+				"id=" + id +
+				", title='" + title + '\'' +
+				", url='" + url + '\'' +
+				", img='" + img + '\'' +
+				", date='" + date + '\'' +
+				", flag=" + flag +
+				'}';
+	}
+	@Generated(hash = 1401345442)
+	public EnglishArticle(int id, String title, String url, String img, String date,
+						  int flag) {
+		this.id = id;
+		this.title = title;
+		this.url = url;
+		this.img = img;
+		this.date = date;
+		this.flag = flag;
+	}
 
+	@Generated(hash = 1122326809)
+	public EnglishArticle() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUrl() {
+		return this.url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getImg() {
+		return this.img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getDate() {
+		return this.date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public int getFlag() {
+		return this.flag;
+	}
+
+	public void setFlag(int flag) {
+		this.flag = flag;
+	}
 }
