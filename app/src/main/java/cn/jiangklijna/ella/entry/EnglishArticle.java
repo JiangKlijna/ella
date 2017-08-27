4b571f7d917f31bcf80f7cddacce7316d9d200f4
jiangKlijna <leil729564090@outlook.com>
2017-08-27 15:06:30 +0800

    :fire:rename & date:string

--- app/src/main/java/cn/jiangklijna/ella/entry/EnglishArticle.java
+++ app/src/main/java/cn/jiangklijna/ella/entry/EnglishArticle.java
@@ -8,19 +8,18 @@ import org.greenrobot.greendao.annotation.Generated;
  * Created by leil7 on 2017/8/22. ella
  */
 @Entity
-public class EnglishCard {
+public class EnglishArticle {
 
     @Id
     private int id;
     private String title;
     private String url;
     private String img;
-    private long date;
+    private String date;
     private int flag;
-
-    @Generated(hash = 759336353)
-    public EnglishCard(int id, String title, String url, String img, long date,
-            int flag) {
+    @Generated(hash = 2078708353)
+    public EnglishArticle(int id, String title, String url, String img, String date,
+                          int flag) {
         this.id = id;
         this.title = title;
         this.url = url;
@@ -28,56 +27,44 @@ public class EnglishCard {
         this.date = date;
         this.flag = flag;
     }
-
     @Generated(hash = 1421194240)
-    public EnglishCard() {
+    public EnglishArticle() {
     }
-
     public int getId() {
         return this.id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
-    public long getDate() {
+    public String getTitle() {
+        return this.title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getUrl() {
+        return this.url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public String getImg() {
+        return this.img;
+    }
+    public void setImg(String img) {
+        this.img = img;
+    }
+    public String getDate() {
         return this.date;
     }
-
-    public void setDate(long date) {
+    public void setDate(String date) {
         this.date = date;
     }
-
     public int getFlag() {
         return this.flag;
     }
-
     public void setFlag(int flag) {
         this.flag = flag;
     }
 
-    public String getTitle() {
-        return this.title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUrl() {
-        return this.url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getImg() {
-        return this.img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
 }
