7c246e8a70da7f7217a8517e28331f49fa3aff09
jiangKlijna <leil729564090@outlook.com>
2017-08-31 17:40:21 +0800

    :fire:use com.facebook.fresco:imagepipeline-okhttp3

--- app/src/main/java/cn/jiangklijna/ella/common/App.kt
+++ app/src/main/java/cn/jiangklijna/ella/common/App.kt
@@ -2,6 +2,8 @@ package cn.jiangklijna.ella.common
 
 import android.app.Application
 import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.imagepipeline.backends.okhttp3.OkHttpImagePipelineConfigFactory
+import okhttp3.OkHttpClient
 
 /**
  * Created by jiangKlijna on 8/10/2017.
@@ -10,7 +12,9 @@ class App : Application() {
 
 	override fun onCreate() {
 		super.onCreate()
-		Fresco.initialize(this)
+
+		val config = OkHttpImagePipelineConfigFactory.newBuilder(this, OkHttpClient()).build()
+		Fresco.initialize(this, config)
 	}
 
 	companion object {
