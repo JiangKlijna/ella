cfa73da2954bb2ddfcf4d03cc5b0b09d75d38aa7
jiangKlijna <leil729564090@outlook.com>
2017-09-12 21:35:26 +0800

    :fire:init lib

--- app/src/main/java/cn/jiangklijna/ella/common/App.kt
+++ app/src/main/java/cn/jiangklijna/ella/common/App.kt
@@ -1,6 +1,8 @@
 package cn.jiangklijna.ella.common
 
 import android.app.Application
+import cn.jiangklijna.ella.entry.DaoMaster
+import cn.jiangklijna.ella.entry.DaoSession
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.facebook.imagepipeline.backends.okhttp3.OkHttpImagePipelineConfigFactory
 import okhttp3.OkHttpClient
@@ -10,15 +12,22 @@ import okhttp3.OkHttpClient
  */
 class App : Application() {
 
-	override fun onCreate() {
-		super.onCreate()
+    override fun onCreate() {
+        super.onCreate()
 
-		val config = OkHttpImagePipelineConfigFactory.newBuilder(this, OkHttpClient()).build()
-		Fresco.initialize(this, config)
-	}
+        // init Fresco
+        val config = OkHttpImagePipelineConfigFactory.newBuilder(this, OkHttpClient()).build()
+        Fresco.initialize(this, config)
 
-	companion object {
-		const val TAG = "ella"
-		const val NAME = "英语听力学习"
-	}
+        // init greeendao
+        val mHelper = DaoMaster.DevOpenHelper(this, "ella", null)
+        val mDaoMaster = DaoMaster(mHelper.writableDatabase)
+        mDaoSession = mDaoMaster.newSession()
+    }
+
+    companion object {
+        const val TAG = "ella"
+        const val NAME = "英语听力学习"
+        var mDaoSession: DaoSession? = null
+    }
 }
