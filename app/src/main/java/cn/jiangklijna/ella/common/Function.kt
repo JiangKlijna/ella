533a5e89e806f02905d2ce1192e64a7a01cee486
jiangKlijna <leil729564090@outlook.com>
2017-09-29 20:01:15 +0800

    :fire:String.md5

--- app/src/main/java/cn/jiangklijna/ella/common/Function.kt
+++ app/src/main/java/cn/jiangklijna/ella/common/Function.kt
@@ -16,52 +16,53 @@ import android.view.View
 import android.widget.Toast
 import com.facebook.drawee.drawable.ScalingUtils
 import com.facebook.drawee.view.DraweeTransition
+import java.security.MessageDigest
 
 /**
  * Created by jiangKlijna on 8/12/2017.
  */
 
 fun <T> T?.println() {
-	android.util.Log.i("App", this?.toString())
+    android.util.Log.i("App", this?.toString())
 }
 
-fun Context.toast(msg: String) {
-	Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+fun Context.toast(msg: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, msg, duration).show()
 }
 
 //获得activity的宽高
 fun Activity.getPixel(): DisplayMetrics = DisplayMetrics().apply {
-	windowManager.defaultDisplay.getMetrics(this)
+    windowManager.defaultDisplay.getMetrics(this)
 }
 
 fun <T : View> View.f(@IdRes id: Int): T = findViewById(id) as T
 
 //设置SharedElement
 fun Activity.setSharedElement() {
-	window.sharedElementEnterTransition = DraweeTransition.createTransitionSet(ScalingUtils.ScaleType.CENTER_CROP, ScalingUtils.ScaleType.FIT_CENTER)
-	window.sharedElementReturnTransition = DraweeTransition.createTransitionSet(ScalingUtils.ScaleType.FIT_CENTER, ScalingUtils.ScaleType.CENTER_CROP)
+    window.sharedElementEnterTransition = DraweeTransition.createTransitionSet(ScalingUtils.ScaleType.CENTER_CROP, ScalingUtils.ScaleType.FIT_CENTER)
+    window.sharedElementReturnTransition = DraweeTransition.createTransitionSet(ScalingUtils.ScaleType.FIT_CENTER, ScalingUtils.ScaleType.CENTER_CROP)
 }
 
 // 根据手机的分辨率从 dp 的单位 转成为 px(像素)
 fun Context.dp2px(dpValue: Float): Int =
-		resources.displayMetrics.density.let {
-			(dpValue * it + 0.5f).toInt()
-		}
+        resources.displayMetrics.density.let {
+            (dpValue * it + 0.5f).toInt()
+        }
 
 
 // 代码设置RippleDrawable
 fun getAdaptiveRippleDrawable(normalColor: Int, pressedColor: Int): Drawable {
-	return RippleDrawable(ColorStateList.valueOf(pressedColor), null, getRippleMask(normalColor))
+    return RippleDrawable(ColorStateList.valueOf(pressedColor), null, getRippleMask(normalColor))
 }
 
 private fun getRippleMask(color: Int): Drawable {
-	// 3 is radius of final ripple,
-	// instead of 3 you can give required final radius
-	val outerRadii = FloatArray(8, { 3f })
-	val r = RoundRectShape(outerRadii, null, null)
-	val shapeDrawable = ShapeDrawable(r)
-	shapeDrawable.paint.color = color
-	return shapeDrawable
+    // 3 is radius of final ripple,
+    // instead of 3 you can give required final radius
+    val outerRadii = FloatArray(8, { 3f })
+    val r = RoundRectShape(outerRadii, null, null)
+    val shapeDrawable = ShapeDrawable(r)
+    shapeDrawable.paint.color = color
+    return shapeDrawable
 }
 
 // 获得版本名
@@ -71,7 +72,14 @@ fun Context.getVersionName(): String = packageManager.getPackageInfo(packageName
 fun Context.getVersionCode(): Int = packageManager.getPackageInfo(packageName, PackageManager.GET_CONFIGURATIONS).versionCode
 
 fun Context.openBrowser(url: String) = Intent().run {
-	action = "android.intent.action.VIEW"
-	data = Uri.parse(url)
-	startActivity(this)
+    action = "android.intent.action.VIEW"
+    data = Uri.parse(url)
+    startActivity(this)
+}
+
+// 获得字符串的md5
+fun String.md5(): CharSequence = StringBuilder().apply {
+    val bytes = toByteArray()
+    val digest = MessageDigest.getInstance("MD5").digest(bytes)
+    for (byte in digest) append("%02x".format(byte))
 }
