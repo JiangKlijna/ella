57adc3660ecde95802764be8fc04f02d01590e36
jiangKlijna <leil729564090@outlook.com>
2017-09-30 20:21:44 +0800

    :fire:adjust field

--- app/src/main/java/cn/jiangklijna/ella/entry/EnglishArticle.java
+++ app/src/main/java/cn/jiangklijna/ella/entry/EnglishArticle.java
@@ -1,8 +1,8 @@
 package cn.jiangklijna.ella.entry;
 
 import org.greenrobot.greendao.annotation.Entity;
-import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.Id;
 
 import java.io.Serializable;
 
@@ -10,88 +10,88 @@ import java.io.Serializable;
  * Created by leil7 on 2017/8/22. ella
  */
 @Entity
-public class EnglishArticle implements Serializable{
+public class EnglishArticle implements Serializable {
 
-	public static final long serialVersionUID = 536871008;
+    public static final long serialVersionUID = 536871008;
 
-	@Id
-	private int id;
-	private String title;
-	private String url;
-	private String img;
-	private String date;
-	private int flag;
-	@Override
-	public String toString() {
-		return "EnglishArticle{" +
-				"id=" + id +
-				", title='" + title + '\'' +
-				", url='" + url + '\'' +
-				", img='" + img + '\'' +
-				", date='" + date + '\'' +
-				", flag=" + flag +
-				'}';
-	}
-	@Generated(hash = 1401345442)
-	public EnglishArticle(int id, String title, String url, String img, String date,
-						  int flag) {
-		this.id = id;
-		this.title = title;
-		this.url = url;
-		this.img = img;
-		this.date = date;
-		this.flag = flag;
-	}
+    @Id
+    private int id;
+    private String title;
+    private String img;
+    private String date;
+    private String sound;
+    private int type;
+    @Override
+    public String toString() {
+        return "EnglishArticle{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", img='" + img + '\'' +
+                ", date='" + date + '\'' +
+                ", sound='" + sound + '\'' +
+                ", type=" + type +
+                '}';
+    }
+    @Generated(hash = 93461174)
+    public EnglishArticle(int id, String title, String img, String date,
+                          String sound, int type) {
+        this.id = id;
+        this.title = title;
+        this.img = img;
+        this.date = date;
+        this.sound = sound;
+        this.type = type;
+    }
 
-	@Generated(hash = 1122326809)
-	public EnglishArticle() {
-	}
+    @Generated(hash = 1122326809)
+    public EnglishArticle() {
+    }
 
-	public int getId() {
-		return this.id;
-	}
+    public int getId() {
+        return this.id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getTitle() {
-		return this.title;
-	}
+    public String getTitle() {
+        return this.title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public String getUrl() {
-		return this.url;
-	}
+    public String getImg() {
+        return this.img;
+    }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    public void setImg(String img) {
+        this.img = img;
+    }
 
-	public String getImg() {
-		return this.img;
-	}
+    public String getDate() {
+        return this.date;
+    }
 
-	public void setImg(String img) {
-		this.img = img;
-	}
+    public void setDate(String date) {
+        this.date = date;
+    }
 
-	public String getDate() {
-		return this.date;
-	}
+    public String getSound() {
+        return this.sound;
+    }
 
-	public void setDate(String date) {
-		this.date = date;
-	}
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
 
-	public int getFlag() {
-		return this.flag;
-	}
+    public int getType() {
+        return this.type;
+    }
 
-	public void setFlag(int flag) {
-		this.flag = flag;
-	}
+    public void setType(int type) {
+        this.type = type;
+    }
 }
