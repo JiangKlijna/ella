21e4ab07e13705038b7acfdf272a64380dbdb898
jiangKlijna <leil729564090@outlook.com>
2017-10-02 10:13:01 +0800

    :fire:create SubTitleView

---
+++ app/src/main/java/cn/jiangklijna/ella/ui/view/SubTitleView.kt
@@ -0,0 +1,20 @@
+package cn.jiangklijna.ella.ui.view
+
+import android.content.Context
+import android.view.View
+import android.widget.LinearLayout
+import cn.jiangklijna.ella.model.Bean
+
+/**
+ * Created by leil7 on 2017/10/2. ella
+ */
+class SubTitleView(context: Context) : LinearLayout(context, null, 0)  {
+
+    init {
+
+    }
+
+    fun set(s: Bean.SubTitle){
+
+    }
+}
