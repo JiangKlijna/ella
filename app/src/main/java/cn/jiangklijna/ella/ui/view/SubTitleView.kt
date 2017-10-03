d6d3db607d28cf68bd4698919d08b39e6a461d3d
jiangKlijna <leil729564090@outlook.com>
2017-10-03 22:15:07 +0800

    :fire:subtitle

--- app/src/main/java/cn/jiangklijna/ella/ui/view/SubTitleView.kt
+++ app/src/main/java/cn/jiangklijna/ella/ui/view/SubTitleView.kt
@@ -1,20 +1,29 @@
 package cn.jiangklijna.ella.ui.view
 
 import android.content.Context
-import android.view.View
+import android.view.LayoutInflater
 import android.widget.LinearLayout
+import android.widget.TextView
+import cn.jiangklijna.ella.R
+import cn.jiangklijna.ella.common.f
 import cn.jiangklijna.ella.model.Bean
 
 /**
  * Created by leil7 on 2017/10/2. ella
  */
-class SubTitleView(context: Context) : LinearLayout(context, null, 0)  {
+class SubTitleView(context: Context) : LinearLayout(context, null, 0) {
+
+    val en: TextView
+    val zh: TextView
 
     init {
-
+        LayoutInflater.from(context).inflate(R.layout.view_subtitle, this, true)
+        en = f(R.id.view_subtitle_en)
+        zh = f(R.id.view_subtitle_zh)
     }
 
-    fun set(s: Bean.SubTitle){
-
+    fun set(s: Bean.SubTitle) {
+        en.text = s.Sentence
+        zh.text = s.Sentence_cn
     }
 }
