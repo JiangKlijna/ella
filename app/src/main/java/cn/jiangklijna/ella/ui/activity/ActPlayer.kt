b89138fd254864e28956ed64d80ede93963987b2
jiangKlijna <leil729564090@outlook.com>
2017-10-04 21:14:49 +0800

    :fire:Requests.listOfSubTitle

--- app/src/main/java/cn/jiangklijna/ella/ui/activity/ActPlayer.kt
+++ app/src/main/java/cn/jiangklijna/ella/ui/activity/ActPlayer.kt
@@ -8,6 +8,9 @@ import cn.jiangklijna.ella.R
 import cn.jiangklijna.ella.common.setSharedElement
 import cn.jiangklijna.ella.common.toast
 import cn.jiangklijna.ella.entry.EnglishArticle
+import cn.jiangklijna.ella.model.Bean
+import cn.jiangklijna.ella.model.Http
+import cn.jiangklijna.ella.model.Requests
 import cn.jiangklijna.ella.ui.dialog.DialogWord
 import kotlinx.android.synthetic.main.act_audio.*
 
@@ -16,41 +19,48 @@ import kotlinx.android.synthetic.main.act_audio.*
  */
 class ActPlayer : AppCompatActivity() {
 
-	var a: EnglishArticle? = null
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setSharedElement()
-		setTheme(R.style.AppTheme_NoActionBar)
-		setContentView(R.layout.act_audio)
-		setSupportActionBar(act_audio_toolbar)
-		a = intent.getSerializableExtra(EnglishArticle::class.java.simpleName) as EnglishArticle
-		act_audio_img.setImageURI(a!!.img)
+    var a: EnglishArticle? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setSharedElement()
+        setTheme(R.style.AppTheme_NoActionBar)
+        setContentView(R.layout.act_audio)
+        setSupportActionBar(act_audio_toolbar)
+        a = intent.getSerializableExtra(EnglishArticle::class.java.simpleName) as EnglishArticle
+        act_audio_img.setImageURI(a!!.img)
 
-		supportActionBar?.setDisplayHomeAsUpEnabled(true)
-		act_audio_toolbar_layout.isTitleEnabled = false
-		supportActionBar?.title = a?.title
-	}
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        act_audio_toolbar_layout.isTitleEnabled = false
+        supportActionBar?.title = a?.title
+        Requests.listOfSubTitle(a!!, run)
+    }
 
-	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-		menuInflater.inflate(R.menu.player, menu)
-		return super.onCreateOptionsMenu(menu)
-	}
+    val run = object : Http.Runnable<List<Bean.SubTitle>> {
+        override fun run(data: List<Bean.SubTitle>) {
 
-	// menu item 的点击事件
-	override fun onOptionsItemSelected(item: MenuItem): Boolean {
-		when (item.itemId) {
-			R.id.menu_total -> {
-			}
-			R.id.menu_search -> {
-				DialogWord(this).show()
-			}
-			R.id.menu_setting -> { // 页面的设置
-				toast("页面的设置")
-			}
-			else -> { // 返回
-				onBackPressed()
-			}
-		}
-		return super.onOptionsItemSelected(item)
-	}
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.player, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    // menu item 的点击事件
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_total -> {
+            }
+            R.id.menu_search -> {
+                DialogWord(this).show()
+            }
+            R.id.menu_setting -> { // 页面的设置
+                toast("页面的设置")
+            }
+            else -> { // 返回
+                onBackPressed()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
