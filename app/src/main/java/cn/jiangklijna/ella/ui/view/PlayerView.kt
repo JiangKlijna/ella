d2f7463d8cb453934ae45316773df237bd7f719f
jiangKlijna <leil729564090@outlook.com>
2017-10-08 21:30:58 +0800

    :fire:add PlayerView

---
+++ app/src/main/java/cn/jiangklijna/ella/ui/view/PlayerView.kt
@@ -0,0 +1,16 @@
+package cn.jiangklijna.ella.ui.view
+
+import android.content.Context
+import android.widget.LinearLayout
+
+/**
+ * Created by leil7 on 2017/10/2. ella
+ */
+class PlayerView(context: Context) : LinearLayout(context, null, 0) {
+
+
+    init {
+//        LayoutInflater.from(context).inflate(R.layout.view_subtitle, this, true)
+    }
+
+}
