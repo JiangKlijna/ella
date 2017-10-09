d6a1c23e38dec2bc71cc53cf9df4ada5ef80f36a
jiangKlijna <leil729564090@outlook.com>
2017-10-09 19:21:57 +0800

    :fire:add view_player.xml

--- app/src/main/java/cn/jiangklijna/ella/ui/view/PlayerView.kt
+++ app/src/main/java/cn/jiangklijna/ella/ui/view/PlayerView.kt
@@ -1,7 +1,9 @@
 package cn.jiangklijna.ella.ui.view
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.widget.LinearLayout
+import cn.jiangklijna.ella.R
 
 /**
  * Created by leil7 on 2017/10/2. ella
@@ -10,7 +12,7 @@ class PlayerView(context: Context) : LinearLayout(context, null, 0) {
 
 
     init {
-//        LayoutInflater.from(context).inflate(R.layout.view_subtitle, this, true)
+        LayoutInflater.from(context).inflate(R.layout.view_player, this, true)
     }
 
 }
