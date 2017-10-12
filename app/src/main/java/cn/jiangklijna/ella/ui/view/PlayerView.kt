0fcc07d3cdeeccc8704003cafab0da74195b3de4
jiangKlijna <leil729564090@outlook.com>
2017-10-12 19:15:22 +0800

    :fire:play

--- app/src/main/java/cn/jiangklijna/ella/ui/view/PlayerView.kt
+++ app/src/main/java/cn/jiangklijna/ella/ui/view/PlayerView.kt
@@ -2,10 +2,12 @@ package cn.jiangklijna.ella.ui.view
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.LinearLayout
 import cn.jiangklijna.ella.R
 import cn.jiangklijna.ella.entry.EnglishArticle
 import cn.jiangklijna.ella.model.Setting
+import kotlinx.android.synthetic.main.view_player.view.*
 import tv.danmaku.ijk.media.player.IjkMediaPlayer
 
 /**
@@ -13,16 +15,19 @@ import tv.danmaku.ijk.media.player.IjkMediaPlayer
  */
 class PlayerView(context: Context) : LinearLayout(context, null, 0) {
 
+    private val player = IjkMediaPlayer()
 
     init {
         LayoutInflater.from(context).inflate(R.layout.view_player, this, true)
     }
 
     fun play(a: EnglishArticle, t: Setting.Type) {
-
+        if (t.isAudio) {
+            view_player_surface.visibility = View.GONE
+        } else {
+            player.setSurface(view_player_surface.holder.surface)
+        }
+        player.dataSource = a.sound
     }
 
-    companion object {
-        private val player = IjkMediaPlayer()
-    }
 }
