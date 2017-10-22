fa64fbc0f76eeb53ec45bd2bb51a64ef91bfbfe3
jiangKlijna <leil729564090@outlook.com>
2017-10-22 11:37:01 +0800

    :fire:add Notification

--- app/src/main/java/cn/jiangklijna/ella/common/PlayerService.kt
+++ app/src/main/java/cn/jiangklijna/ella/common/PlayerService.kt
@@ -1,12 +1,18 @@
 package cn.jiangklijna.ella.common
 
+import android.app.Notification
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.IBinder
+import android.support.v4.app.NotificationCompat
+import cn.jiangklijna.ella.R
 import cn.jiangklijna.ella.entry.EnglishArticle
 import org.greenrobot.eventbus.EventBus
 import tv.danmaku.ijk.media.player.IjkMediaPlayer
+
 
 /**
  * Created by leil7 on 2017/10/21. ella
@@ -15,6 +21,7 @@ class PlayerService : Service() {
 
     var a: EnglishArticle? = null
     var player: IjkMediaPlayer? = null
+    var nf: NotificationCompat.Builder? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
@@ -39,6 +46,27 @@ class PlayerService : Service() {
             prepareAsync()
             start()
         }
+        initNotification(a)
+        notify(nf!!)
+    }
+
+    private fun initNotification(a: EnglishArticle) {
+        if (nf == null) {
+            nf = NotificationCompat.Builder(this)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+//                  .setContentIntent(PendingIntent.getActivity(this, 0, Intent(), 0))
+                    .setContentTitle(a.title)
+                    .setTicker(a.title)
+                    .setAutoCancel(false)
+                    .setVisibility(Notification.VISIBILITY_PUBLIC)
+        }
+        nf!!.setProgress(100, 0, false)
+    }
+
+    private fun notify(nf: NotificationCompat.Builder) {
+        val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        nm.notify(0, nf.build())
     }
 
     override fun onDestroy() {
